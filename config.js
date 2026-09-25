47311494d455405630d3b41b63747b619235a724
narlan77 <44502388+narlan77@users.noreply.github.com>
2026-09-25 18:08:03 +0700

    Update API_URL to new endpoint

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // CONFIGURASI UTAMA API
-const API_URL = "https://script.google.com/macros/s/AKfycby37_1vvcjZExq5CIMDkDbiVAEra71b_oincEbbnmk0MS7_nlQ78u4qRg61IWX7jWECwQ/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbwBhgD98Pk-FBCR8xP3FaCIg5gzVnPzOVVdsd_ErwZ7dD202j3LXz1x0YElsEVW_6jIlw/exec";
